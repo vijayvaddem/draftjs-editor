8a990c2137aaccfa7958aef4456642f3345c4c49
vaddv <vijay.vaddem@gmail.com>
2020-07-22 22:37:27 +0530

    Added Editor component from draft.js

---
+++ src/MyEditor.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
+
+class MyEditor extends React.Component {
+  editorStyles = {
+    width: "90%",
+    height: "50%",
+    margin: "10px",
+    border: "1px solid red",
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = (editorState) => this.setState({ editorState });
+  }
+
+  handleKeyCommand(command) {
+    const { editorState } = this.state;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    return (
+      <div style={this.editorStyles}>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyEditor;
